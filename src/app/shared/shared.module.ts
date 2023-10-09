6c74697356de7e8f920228f57ffc755ed9b5c5b6
Steven Lizarzaburu Pezúa <lizarzaburupezuasteven@gmail.com>
2023-10-08 23:25:46 -0500

    Creacion de los pipes - filtros

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,10 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { CardPlayerComponent } from './components/card-player/card-player.component';
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
+import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
+import { RouterModule } from '@angular/router';
+import { OrderListPipe } from './pipe/order-list.pipe';
 
 
 
@@ -16,9 +20,14 @@ import { SectionGenericComponent } from './components/section-generic/section-ge
        MediaPlayerComponent,
        CardPlayerComponent,
        SectionGenericComponent,
+       PlayListHeaderComponent,
+       PlayListBodyComponent,
+       OrderListPipe,
+       
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SideBarComponent,
@@ -27,6 +36,9 @@ import { SectionGenericComponent } from './components/section-generic/section-ge
     HeaderUserComponent,
     CardPlayerComponent,
     SectionGenericComponent,
+    PlayListHeaderComponent,
+    PlayListBodyComponent,
+    OrderListPipe
   ]
 })
 export class SharedModule { }
